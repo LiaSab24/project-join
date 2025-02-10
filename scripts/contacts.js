214f36262c3bfde8f1e9758f31143e3a1792aa7e
Anne Vollmann <vollmann.anne@web.de>
2025-02-10 14:31:39 +0100

    contacts-overlay-finished

---
+++ scripts/contacts.js
@@ -0,0 +1,7 @@
+function contactClicked(id) {
+    for (let indexContacts = 0; indexContacts < contacts.length; indexContacts++) {
+        //remove "contact-clicked" class for all contacts;
+    }
+    //add "contact-clicked" class to clicked contact
+    //show "focused-contact-information" for clicked contact
+}
