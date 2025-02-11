e070625b13f2c68a7ee962f13009a2cffb8fbd6c
Anne Vollmann <vollmann.anne@web.de>
2025-02-11 10:48:34 +0100

    hover-effects-and-toggle-overlay-animation

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -5,3 +5,10 @@ function contactClicked(id) {
     //add "contact-clicked" class to clicked contact
     //show "focused-contact-information" for clicked contact
 }
+
+function toggleOverlay() {
+    let overlayContentRef = document.getElementById("overlayContact");
+    overlayContentRef.classList.toggle("animation-open-overlay");
+    setTimeout(function() { overlayContentRef.classList.toggle("d-none"); }, 300);
+    overlayContentRef.classList.toggle("animation-close-overlay");
+}
