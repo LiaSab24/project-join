eeffac3b7e40642034290561b84da981aaccde68
Anne Vollmann <vollmann.anne@web.de>
2025-02-13 11:42:14 +0100

    test-data(10-contacts,5-tasks,2-users)

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -11,8 +11,8 @@ function toggleOverlay() {
     let overlayContactContentRef = document.getElementById("overlayContact");
     overlayContactContentRef.classList.toggle("animation-open-overlay");
     overlayContactContentRef.classList.toggle("animation-close-overlay");
-    setTimeout(function() { 
-        overlayContactContentRef.classList.toggle("d-none"); 
+    setTimeout(function () {
+        overlayContactContentRef.classList.toggle("d-none");
         overlayBgContentRef.classList.toggle("d-none");
     }, 300);
     //change content of overlay, depending of clicked button (add or edit)
@@ -22,8 +22,51 @@ function contactSuccesfullyCreated() {
     let successAnimation = document.getElementById("contactSuccesfullyCreated");
     successAnimation.style.animationName = "contactSuccesfullyCreated";
     successAnimation.style.animationDuration = "1600ms";
-    setTimeout(function() { 
+    setTimeout(function () {
         successAnimation.style.animationName = "";
         successAnimation.style.animationDuration = "";
     }, 1600);
+}
+
+// TEST-DATEN
+
+function addTestContact() {
+    let testContactName = document.getElementById("testcontactname").innerHTML;
+    let testContactMail = document.getElementById("testcontactmail").innerHTML;
+    let testContactPhone = document.getElementById("testcontactphone").innerHTML;
+    postData("/contacts/", {
+        "name": testContactName,
+        "mail": testContactMail,
+        "phone": testContactPhone
+    });
+}
+
+function addTestTask() {
+    let testTaskTitle = document.getElementById("testtaskstitle").innerHTML;
+    let testTaskDescription = document.getElementById("testtasksdescrip").innerHTML;
+    let testTaskAssignedTo = document.getElementById("testtasksassign").innerHTML;
+    let testTaskDueDate = document.getElementById("testtasksdue").innerHTML;
+    let testTaskPriority = document.getElementById("testtasksprio").innerHTML;
+    let testTaskCategory = document.getElementById("testtaskscate").innerHTML;
+    let testTaskSubtasks = document.getElementById("testtaskssub").innerHTML;
+    postData("/tasks/", {
+        "title": testTaskTitle,
+        "description": testTaskDescription,
+        "assignedTo": testTaskAssignedTo,
+        "dueDate": testTaskDueDate,
+        "priority": testTaskPriority,
+        "category": testTaskCategory,
+        "subtasks": testTaskSubtasks
+    });
+}
+
+function addTestUser() {
+    let testUserName = document.getElementById("testusername").innerHTML;
+    let testUserMail = document.getElementById("testusermail").innerHTML;
+    let testUserPassword = document.getElementById("testuserpass").innerHTML;
+    postData("/users/", {
+        "name": testUserName,
+        "mail": testUserMail,
+        "password": testUserPassword
+    });
 }
