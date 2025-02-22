51e4994812f4ee45a251776787384307ef4b015e
LiaSab24 <164032382+LiaSab24@users.noreply.github.com>
2025-02-22 17:12:01 +0100

    added something

--- script.js
+++ script.js
@@ -1,20 +1,7 @@
-let getSubmenu = document.getElementById('submenu');
-
-function btnUserInitial(){
-  getSubmenu.innerHTML = '';
-  getSubmenu.classList.remove('d-none');
-  getSubmenu.innerHTML += getSubmenuHTML();
-}
-
-// kommt später in die templates.js Datei
-function getSubmenuHTML(){
-  return /*html*/`
-  <p><a href="../html/legal_note.html">Legal Notice</a></p>
-  <p><a href="../html/privacy_police.html">Privacy Policy</a></p>
-  <p><a href="../html/signup.html">Logout</a></p>
-  `;
-}
-
-function closeSubmenu() {
-  getSubmenu.classList.add('d-none');
+function handleResize() {
+  if (window.innerWidth <= 780) {
+    help?.classList.remove('d-none');
+  } else {
+    help?.classList.add('d-none');
+  }
 }
