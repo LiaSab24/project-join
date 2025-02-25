e9390239958cf2f84bc7f7050d1bb49155f2521c
SonjaReimannThanisch <137377644+Sonja40@users.noreply.github.com>
2025-02-25 12:10:07 +0100

    new onclick funktion on edit btn on User Feedaback task card

--- scripts/board_template.js
+++ scripts/board_template.js
@@ -106,7 +106,7 @@ function createFeedbackOverlay() {
                     <img src="/assets/icons/delete.png" alt="Delete-Icon"> Delete
                 </button>
                 <div class="divider"></div>
-                <button class="feedback-edit-btn">
+                <button class="feedback-edit-btn" onclick="editFeedbackCard()">
                     <img src="/assets/icons/edit.png" alt="Edit-Icon"> Edit
                 </button>
             </div>
