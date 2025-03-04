f732abfcab3eb4008b4c31f98b16df33cb8d1a38
LiaSab24 <164032382+LiaSab24@users.noreply.github.com>
2025-03-04 11:27:21 +0100

    corrected include.js

--- scripts/include.js
+++ scripts/include.js
@@ -24,30 +24,3 @@ function includeHTML() {
     resolve(); // Promise wird aufgelöst, wenn die Einbindung abgeschlossen ist
   });
 }
-
-
-
-/*
-async function includeHTML() {
-  const elements = document.querySelectorAll("[w3-include-html]");
-
-  const fetchAndInclude = async (element) => {
-    const file = element.getAttribute("w3-include-html");
-    if (!file) return;
-
-    try {
-      const response = await fetch(file);
-      if (!response.ok) {
-        throw new Error("Page not found.");
-      }
-      element.innerHTML = await response.text();
-    } catch (error) {
-      element.innerHTML = error.message;
-    } finally {
-      element.removeAttribute("w3-include-html");
-    }
-  };
-
-  await Promise.all([...elements].map(fetchAndInclude));
-}
-*/
