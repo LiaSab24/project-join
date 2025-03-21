cb3288dcfb62a6ce5d0f0386cc52c66343fde07b
Sonja Reimann Thanisch <137377644+SonjaReimannThanisch@users.noreply.github.com>
2025-03-21 10:47:34 +0100

    deleted glow and fade in and added a grey automatically high borader

--- scripts/board.js
+++ scripts/board.js
@@ -223,7 +223,6 @@ function filterTasks(searchInput) {
   }
 }
 
-
 /**
  * shows the position, where the elemnt would be visible if the user drops it
  * 
@@ -256,8 +255,6 @@ function hideDropdownArea(contentRefId) {
 function drag(event) {
   event.dataTransfer.setData("text", event.target.id);
 }
-
-
 
 /**
  * Handles the drop event by moving the dragged task to a new column and updating its progress.
